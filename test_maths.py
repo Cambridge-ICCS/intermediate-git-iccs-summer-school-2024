a633207dc5699510a0ff415f4f802a78837be1fd
Joe Wallwork <jw2423@cam.ac.uk>
2024-06-12 13:06:20 +0100

    TDD: basic tests equation_of_line must satisfy

---
+++ test_maths.py
@@ -0,0 +1,18 @@
+from maths import *
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize(
+    "a,b,condition",
+    [
+        ((0, 0), (0, 1), lambda x, y: x == 0),
+        ((0, 0), (1, 0), lambda x, y: y == 0),
+        ((0, 0), (1, 1), lambda x, y: x == y),
+    ]
+)
+def test_equation(a, b, condition):
+    f = equation_of_line(a, b)
+    for (x, y) in [(i, j) for j in range(10) for i in range(10)]:
+        iszero = np.isclose(f(x, y), 0)
+        assert iszero if condition(x, y) else not iszero

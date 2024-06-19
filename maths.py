ad7852a5e4ee53a438af95e1aed112664f08d7f9
Joe Wallwork <jw2423@cam.ac.uk>
2024-06-19 09:52:58 +0100

    Implement equation_of_line using SymPy

--- maths.py
+++ maths.py
@@ -1,3 +1,6 @@
+from sympy import Line, Point2D
+
+
 def equation_of_line(a, b):
     """
     Determine the equation of the line passing through two points.
@@ -6,20 +9,9 @@ def equation_of_line(a, b):
     :arg b: the second point the line passes through
     :return: function of two variables defining the line
     """
-    x0, y0 = a
-    x1, y1 = b
+    line = Line(Point2D(a), Point2D(b))
 
-    if (x0 == x1):
-        # Special case of a vertical line
+    def evaluate(x, y):
+        return float(line.distance(Point2D((x, y))))
 
-        def f(x, y):
-            return x - x0
-
-    else:
-
-        def f(x, y):
-            m = (y1 - y0) / (x1 - x0)
-            c = y0 - m * x0
-            return y - m * x - c
-
-    return f
+    return evaluate
